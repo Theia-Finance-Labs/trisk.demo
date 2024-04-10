e5d0a9dca1e2a29e11af6dcdc3c952590b7ae487
bertrand <bertrand.gallice.pro@gmail.com>
2024-04-10 16:15:51 +0200

    basic demo + multirun

---
+++ trisk_multirun.R
@@ -0,0 +1,27 @@
+library(r2dii.climate.stress.test)
+
+project_name <- "trisk_runs"
+
+input_path <- r2dii.utils::path_dropbox_2dii(fs::path("ST Inputs","ST_INPUTS_MASTER"))
+output_dir_trisk <- fs::path(project_name, "st_outputs")
+
+
+run_params = list(
+  list(
+    scenario_geography="Global",
+    baseline_scenario="WEO2021_APS",
+    shock_scenario="WEO2021_SDS",
+    shock_year=2030
+  ),
+  list(
+    scenario_geography="India",
+    baseline_scenario="WEO2021_APS",
+    shock_scenario="WEO2021_SDS",
+    shock_year=2030
+  )
+)
+
+
+for (run_param in run_params){
+  do.call(run_trisk, c(list(output_path = output_dir_trisk, input_path = input_path), run_param))
+}
